34ad4197e1d80efdaacef0b0981550f0de6bb715
Yurui Zhang <eraclaymore@hotmail.com>
2017-10-25 15:55:35 -0400

    [#46] adds chinese (mandarin) translation
    Resolves #46
    closes PR https://github.com/mirai-audio/mir/pull/49

---
+++ app/locales/zh-cn/translations.js
@@ -0,0 +1,16 @@
+/* eslint-disable quotes, quote-props */
+import app from './app';
+import about from './about';
+import common from './common';
+import components from './components';
+import errors from './errors';
+import routes from './routes';
+
+export default {
+  'app': app,
+  'about': about,
+  'common': common,
+  'components': components,
+  'errors': errors,
+  'routes': routes,
+};
