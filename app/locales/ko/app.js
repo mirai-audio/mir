e77c24a6c7d3e10e41275031fad9ba01b243e8a3
Derrick Lim <lim.derrick@gmail.com>
2017-10-25 16:01:11 +0800

    Began work on translating to Korean

---
+++ app/locales/ko/app.js
@@ -0,0 +1,18 @@
+/* eslint-disable quotes, quote-props */
+
+export default {
+  'network': {
+    'online': '온라인',
+    'offline': '오프라인',
+    'offline-detail': '연결 없음',
+  },
+
+  'noscript': {
+    'title': 'mirai.audio는 JavaScript에서 우수합니다',
+    'copy': 'mirai.audio 팀은 JavaScript를 무효로한다는 결정을 존중합니다.' +
+      '재생 / 일시 정지 및 오프라인 기능을 사용하려면 JavaScript가 필요합니다. mirai.audio' +
+      '팀은 JavaScript를 활성화하지' +
+      '<a href="https://github.com/mirai-audio/mir/wiki/Browser-Support">지원</a> ' +
+      '하기 위해 노력하고 있습니다.',
+  },
+};
