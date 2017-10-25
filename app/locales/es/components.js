e060e5cbbf2e254a076da1693744912feb333088
andresluna11 <32135066+andresluna11@users.noreply.github.com>
2017-10-25 22:26:55 +0200

    Create components.js (Translated to spanish)
    components.js translated to spanish

---
+++ app/locales/es/components.js
@@ -0,0 +1,16 @@
+/* eslint-disable quotes, quote-props */
+
+export default {
+  'ma-auth': {
+    'socialTitle': 'Entra con',
+    'title': 'O Registrate',
+  },
+
+  'ma-header': {
+    'back': 'Atrás',
+  },
+
+  'ma-login': {
+    'cta-login': 'Entra',
+  },
+};
