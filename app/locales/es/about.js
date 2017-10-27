2bc9939b6a1a4987e6569e4d1aa766722a81ba99
EnrikeGonzaloLegarra <enrikeglegarra@gmail.com>
2017-10-27 23:33:34 +0200

    translation es

---
+++ app/locales/es/about.js
@@ -0,0 +1,10 @@
+/* eslint-disable quotes, quote-props */
+
+export default {
+    'copy': 'La aplicación de audio para unir todos los servicios de transmisión de música. Reproducir canciones ' +
+        'de Spotify, Last.fm, Soundcloud, YouTube, mp3s. Crea listas de reproducción' +
+        'en servicios como YouTube, Soundcloud, Spotify, Last.fm y más. ' +
+        'Crea y comparte listas de reproducción en todos los servicios. Seguir a los artistas y promover ' +
+        'canciones.',
+    'arriving': 'Llegando a 2017',
+};
---
+++ app/locales/es/components.js
@@ -0,0 +1,16 @@
+/* eslint-disable quotes, quote-props */
+
+export default {
+    'ma-auth': {
+        'socialTitle': 'Registrarse con ',
+        'title': 'O regístrate',
+    },
+
+    'ma-header': {
+        'back': 'Atras',
+    },
+
+    'ma-login': {
+        'cta-login': 'Registrarse',
+    },
+};
