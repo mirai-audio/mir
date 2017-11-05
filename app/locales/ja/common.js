b4f27591bc15d3adec7d28b30fba6e41e0a91ff1
0xADADA <0xadada.pub@0xadada.pub>
2017-11-04 21:02:52 -0400

    Added application loading/error substates

--- app/locales/ja/common.js
+++ app/locales/ja/common.js
@@ -3,8 +3,10 @@
 export default {
   'appname': 'mirai.audio',
   'tagline': 'すべてをプレイし、どこでも遊ぶ',
+
+  'close': '却下する',
+  'loading': 'お待ちください…',
   'logout': 'ログアウト',
-  'close': 'Close',
 
   'svgjar': 'svg-jar',
 };
