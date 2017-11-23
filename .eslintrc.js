ff2907fee0c2df376fac19b54be8066b949abb9a
0xADADA <0xadada.pub@0xadada.pub>
2017-11-23 10:22:32 -0500

    cleaned up eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,51 +4,47 @@ module.exports = {
     ecmaVersion: 2017,
     sourceType: 'module'
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:ember/recommended',
-  ],
-  'plugins': [
-    'prettier',
-  ],
+  plugins: ['ember', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:ember/recommended'],
   env: {
     browser: true,
-    node: true,
+    node: true
   },
   globals: {
-    'expectAssertion': true,
-    'expectDeprecation': true,
-    'expectNoDeprecation': true,
-    'expectWarning': true,
-    'expectNoWarning': true,
-    'ignoreAssertion': true,
-    'ignoreDeprecation': true,
+    expectAssertion: true,
+    expectDeprecation: true,
+    expectNoDeprecation: true,
+    expectWarning: true,
+    expectNoWarning: true,
+    ignoreAssertion: true,
+    ignoreDeprecation: true,
 
     // required for ember-cli-mirage
-    'server': true,
+    server: true,
 
     // A safe subset of 'browser:true':
-    'window': true,
-    'console': true,
-    'document': true,
-    'Symbol': true,
-    'WeakMap': true,
+    window: true,
+    console: true,
+    document: true,
+    Symbol: true,
+    WeakMap: true
   },
   rules: {
     // enforce
-    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'no-console': [2, { allow: ['warn', 'error'] }],
+
     // prettier
-    'prettier/prettier': 'error',
+    'prettier/prettier': 2,
+
     // eslint-plugin-ember
-    'ember/no-old-shims': 'error',
-    'ember/new-module-imports': 'error',
-    'ember/no-jquery': 'error',
-    'ember/local-modules': 'warn',
+    'ember/no-old-shims': 2,
+    'ember/new-module-imports': 2,
+    'ember/no-jquery': 2,
+    'ember/local-modules': 1,
+    'ember/alias-model-in-controller': 1,
+    'ember/named-functions-in-promises': 0,
 
     // relax
-    'no-underscore-dangle': 0,
-    // eslint-plugin-ember
-    'ember/alias-model-in-controller': 'warn',
-    'ember/named-functions-in-promises': 0,
+    'no-underscore-dangle': 0
   }
 };
--- tests/.eslintrc.js
+++ tests/.eslintrc.js
@@ -2,20 +2,18 @@ module.exports = {
   env: {
     embertest: true
   },
+  extends: '../.eslintrc.js',
   rules: {
-    // enforce
-    'ember/no-old-shims': 'error',
-    'ember/new-module-imports': 'error',
-    'ember/no-jquery': 'warn',
-
     // relax
-    'comma-dangle': 0,
     'func-names': 0,
     'prefer-const': 0,
     'prefer-arrow-callback': 0,
     'space-before-function-paren': 0,
+
+    // eslint-plugin-ember
+    'ember/no-jquery': 1,
     'ember/use-ember-get-and-set': 0,
     'ember/avoid-leaking-state-in-components': 0,
-    'ember/named-functions-in-promises': 'off',
-  },
+    'ember/named-functions-in-promises': 0
+  }
 };
