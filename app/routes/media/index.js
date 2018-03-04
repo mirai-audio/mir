fd14ae0c05ca396e93281e5495616370a75ca3ca
0xADADA <0xadada.pub@0xadada.pub>
2018-03-03 22:50:29 -0500

    added /media/:media_id to authenticate routes

--- app/routes/media/index.js
+++ app/routes/media/index.js
@@ -1,7 +1,8 @@
 import Route from '@ember/routing/route';
 import { get } from '@ember/object';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Route.extend({
+export default Route.extend(AuthenticatedRouteMixin, {
   async model(params) {
     let mediaId = get(params, 'media_id');
     let store = get(this, 'store');
