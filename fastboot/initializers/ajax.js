4003e071901dd1fdd8806075f8c3937a0bf59a52
0xADADA <0xadada.pub@0xadada.pub>
2018-03-08 17:20:25 -0500

    [#31] added fastboot initializer
    * [ ] get Mirage to work with ember-fetch

---
+++ fastboot/initializers/ajax.js
@@ -0,0 +1,9 @@
+export default {
+  name: 'ajax-service',
+  initialize() {
+    /* noop
+     * This is to override Fastboot's initializer which prevents ember-fetch from working
+     * https://github.com/ember-fastboot/ember-cli-fastboot/blob/master/fastboot/initializers/ajax.js
+     */
+  }
+};
