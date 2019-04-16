b0cbe44a96b43fd5bb2295ad8eca2e1079de7338
0xADADA <0xadada.pub@0xadada.pub>
2019-04-15 20:58:01 -0400

    updated bulma, ember-changeset-validations

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -1,8 +1,5 @@
 /* global window */
 window.deprecationWorkflow = window.deprecationWorkflow || {};
 window.deprecationWorkflow.config = {
-  workflow: [
-    { handler: 'silence', matchId: 'ember-map-deprecation' },
-    { handler: 'silence', matchId: 'ember-runtime.deprecate-copy-copyable' }
-  ]
+  workflow: []
 };
