7fe31d783b826648d5c7f67974db2dc2dbd1d1f4
0xADADA <radams@salsify.com>
2019-04-29 12:17:58 -0400

    fix for youtube video component

--- tests/integration/components/ma-media-detail-test.js
+++ tests/integration/components/ma-media-detail-test.js
@@ -2,6 +2,7 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import EmberObject from '@ember/object';
+import { later } from '@ember/runloop';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | ma-media-detail', function(hooks) {
@@ -29,5 +30,18 @@ module('Integration | Component | ma-media-detail', function(hooks) {
     assert.ok(content.includes('asdf'), 'includes title');
     assert.ok(content.includes('example.com'), 'includes url');
     assert.ok(content.includes('template block text'), 'includes block text');
+  });
+
+  test('it renders a youtube video', async function(assert) {
+    let done = assert.async();
+
+    let model = 'hpDvtIt6Lsc';
+    this.set('model', model);
+    await render(hbs`{{ember-youtube ytid=model}}`);
+    later(() => {
+      assert.dom('iframe').exists();
+      assert.dom('iframe').hasAttribute('src');
+      done();
+    }, 500);
   });
 });
