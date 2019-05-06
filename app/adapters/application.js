4babe1050255c24c01dc8425714b1cb4bb912fef
0xADADA <0xadada.pub@0xadada.pub>
2019-05-05 21:23:37 -0400

    disable service worker in dev mode

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,7 @@
 import JSONAPIAdapter from 'ember-data/adapters/json-api';
 import AdapterFetchMixin from 'ember-fetch/mixins/adapter-fetch';
 import config from 'mir/config/environment';
-import { computed, get } from '@ember/object';
+import { computed } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default JSONAPIAdapter.extend(AdapterFetchMixin, {
@@ -11,22 +11,6 @@ export default JSONAPIAdapter.extend(AdapterFetchMixin, {
   host: config.DS.host,
   namespace: config.DS.namespace,
 
-  /*
-  authorizer: 'authorizer:oauth2-bearer',
-  */
-
-  /* mir */
-  zzajaxOptions() {
-    const authorizer = get(this, 'authorizer');
-    const options = this._super(...arguments) || {};
-    options.headers = options.headers || {};
-    options.headers['Content-Type'] = 'application/vnd.api+json';
-    get(this, 'session').authorize(authorizer, (headerName, headerValue) => {
-      options.headers[headerName] = headerValue;
-    });
-    return options;
-  },
-
   headers: computed('session.data.authenticated.token', function() {
     const headers = {};
     headers['Content-Type'] = 'application/vnd.api+json';
@@ -34,7 +18,6 @@ export default JSONAPIAdapter.extend(AdapterFetchMixin, {
       const token = this.session.data.authenticated['access_token'];
       headers['Authorization'] = `Bearer ${token}`;
     }
-
     return headers;
   })
 });
